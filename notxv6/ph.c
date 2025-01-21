cbb8e0363978faddcd69ca71d75b9ff8c6ea5cbd
wasabijc <1582850864@qq.com>
2025-01-21 15:52:54 +0800

    NEW FEATURE:Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lockarr[NBUCKET];//为每个bucket设置一个锁
 
 double
 now()
@@ -41,6 +41,8 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lockarr[i]);//加锁
+
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -55,6 +57,7 @@ void put(int key, int value)
     insert(key, value, &table[i], table[i]);
   }
 
+  pthread_mutex_unlock(&lockarr[i]);//解锁
 }
 
 static struct entry*
@@ -62,12 +65,14 @@ get(int key)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lockarr[i]);//加锁
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
 
+  pthread_mutex_unlock(&lockarr[i]);//解锁
   return e;
 }
 
@@ -104,7 +109,8 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+  for(int i = 0; i < NBUCKET; i++)
+   pthread_mutex_init(&lockarr[i], NULL);//初始化锁
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
